8b96299bdb8a2cc3242b60ebb186a1dea7576be2
Q <coderinch@gmail.com>
2021-05-29 18:41:48 +0900

    add another method of randomizing dice number under the commenting

--- dice app/dice app/ViewController.swift
+++ dice app/dice app/ViewController.swift
@@ -19,14 +19,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         diceImageOne.image=#imageLiteral(resourceName: "five")
         diceImageTwo.image=#imageLiteral(resourceName: "one")
-        
     }
 
     @IBAction func rollButton(_ sender: UIButton) {
         let imageArray=[#imageLiteral(resourceName: "one"),#imageLiteral(resourceName: "two"),#imageLiteral(resourceName: "three"),#imageLiteral(resourceName: "four"),#imageLiteral(resourceName: "five"),#imageLiteral(resourceName: "six")]
         diceImageOne.image=imageArray[Int.random(in:0...5)]
         diceImageTwo.image=imageArray[Int.random(in:0...5)]
-    
+
+//        another way to solve this rolling the dice
+//        let imageNameArray = ["one","two","three","four","five","six"]
+//        diceImageOne.image = UIImage(named:imageNameArray[Int.random(in:0...5)])
+//        diceImageTwo.image = UIImage(named:imageNameArray[Int.random(in:0...5)])
     }
     
 }
